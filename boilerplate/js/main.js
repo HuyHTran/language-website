15519eefb10b64cd4467c98fa4c8a8fbe20100de
Huy <huyhctran@gmail.com>
2015-07-23 17:50:51 -0400

    added flipbook javascript game

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,10 +1,28 @@
-$(document).ready(function(){
-           // set up hover panels
-           // although this can be done without JavaScript, we've attached these events
-          // because it causes the hover to be triggered when the element is tapped on a touch device
-        $('.hover').hover(function(){
-            $(this).addClass('flip');
-        },function(){
-            $(this).removeClass('flip');
-        });
-    });
+// flipbook code
+var numPanels = $('.panel').length;
+
+// if a panel is open, lower its z-idx
+// otherwise, set zIdx back to original
+function checkZ($aPanel) {
+  if ( $aPanel.hasClass('open') ) {
+    $aPanel.css('z-index','1');
+  } else {
+    // set z-index back to original stored in data
+    zIdx = $aPanel.data('zIdx');
+    $aPanel.css( 'z-index', zIdx );
+  }
+}
+
+// loop through all panels and reverse sort via zIdx
+for (i=0; i<(numPanels); i++  ) {
+  var zIdx =  numPanels-i;
+  $('.panel').eq(i).css('z-index',zIdx).data('zIdx',zIdx);
+}
+
+// when clicking the front panel add class 'open' to panel
+// if clicking bacl panel, remove 'open' from panel
+$('.panel').on('click', '.front, .back', function() {
+  $(this).parent('.panel').toggleClass('open');
+  checkZ($(this).parent('.panel'));
+});
+// end flipbook code
